5c5336892a8ccf8a9274132e3ce7d95e31e0c7bc
shivamanipatil <shivamanipatil10@gmail.com>
2020-11-29 14:19:14 +0400

    Forward and backward pass functions for Viterbi

--- src/scripts/viterbi.py
+++ src/scripts/viterbi.py
@@ -43,3 +43,63 @@ class Viterbi():
         # Backward step
         self.backward()
         return self.X
+
+    def init(self):
+        """
+        Initialize start probabilities
+        """
+        s_idx = self.S.index("--s--")
+        for i in range(self.K):
+            if self.A[s_idx][i] == 0:
+                self.T1[i][0] = float("-inf")
+                self.T2[i][0] = 0
+            else:
+                self.T1[i][0] = \
+                    math.log(self.A[s_idx][i]) + \
+                    math.log(self.B[i][self.lookup[self.Y[0]]])
+                self.T2[i][0] = 0
+
+    def forward(self):
+        """
+        Forward step
+        """
+        for i in range(1, self.T):
+
+            if i % 5000 == 0:
+                print("Words processed: {:>8}".format(i))
+
+            for j in range(self.K):
+
+                best_prob = float("-inf")
+                best_path = None
+
+                for k in range(self.K):
+
+                    prob = self.T1[k][i - 1] + \
+                        math.log(self.A[k][j]) + \
+                        math.log(self.B[j][self.lookup[self.Y[i]]])
+
+                    if prob > best_prob:
+                        best_prob = prob
+                        best_path = k
+
+                self.T1[j][i] = best_prob
+                self.T2[j][i] = best_path
+
+    def backward(self):
+        """
+        Backward step
+        """
+        z = [None] * self.T
+        argmax = self.T1[0][self.T - 1]
+
+        for k in range(1, self.K):
+            if self.T1[k][self.T - 1] > argmax:
+                argmax = self.T1[k][self.T - 1]
+                z[self.T - 1] = k
+
+        self.X[self.T - 1] = self.S[z[self.T - 1]]
+
+        for i in range(self.T - 1, 0, -1):
+            z[i - 1] = self.T2[z[i]][i]
+            self.X[i - 1] = self.S[z[i - 1]]
